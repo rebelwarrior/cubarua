a3004a7de05ad32b7aee69960f19e31f28de9847
David Acevedo <rabbit.editor@gmail.com>
2013-03-08 21:31:04 -0400

    added version and api route

--- cubarua.rb
+++ cubarua.rb
@@ -18,33 +18,35 @@ Cuba.define do
     on root do
       res.write "Hello Puerto Rico!" + Cuba.settings.to_s
     end
-    on "all", param('callback') do
-      if param('callback')
-        puts "___________________"
-        puts param('callback')
-        puts "-------------------"
-        res.write (param('callback')['json' => Agency.all.to_json])     
-      else
-        res.write Agency.all.to_a.to_json
+    on "api/v1" do
+      on "all", param('callback') do
+        if param('callback')
+          puts "___________________"
+          puts param('callback')
+          puts "-------------------"
+          res.write (param('callback')['json' => Agency.all.to_json])     
+        else
+          res.write Agency.all.to_a.to_json
+        end
+        # res.write render(Agency.all.to_json, :callback => params[:callback])
       end
-      # res.write render(Agency.all.to_json, :callback => params[:callback])
-    end
-    on "id/:id" do |id|
-      agency_ref_number = Agency.find_by_ref_number(id)
-      res.write agency_ref_number.to_json
-    end
-    on "search/:query" do |query|
-      q = "%#{sanitize(query)}%"
-      # puts "Recieved / query: #{query[0..22]} Sanitized: #{q} At #{Time.now}"
-      agency_name_search_results = Agency.where('lower(agency_name) LIKE ?', q)
-      res.write agency_name_search_results.to_json     
-    end
-    #search?q=DeptRedundacy
-    on "search", param('q') do |query|
-      q = "%#{sanitize(query)}%"
-      # puts "Search ?q= Query: #{query[0..22]} Sanitized: #{q} At #{Time.now}"
-      agency_name_search_results = Agency.where('lower(agency_name) LIKE ?', q)
-      res.write agency_name_search_results.to_json     
+      on "id/:id" do |id|
+        agency_ref_number = Agency.find_by_ref_number(id)
+        res.write agency_ref_number.to_json
+      end
+      on "search/:query" do |query|
+        q = "%#{sanitize(query)}%"
+        # puts "Recieved / query: #{query[0..22]} Sanitized: #{q} At #{Time.now}"
+        agency_name_search_results = Agency.where('lower(agency_name) LIKE ?', q)
+        res.write agency_name_search_results.to_json     
+      end
+      #search?q=DeptRedundacy
+      on "search", param('q') do |query|
+        q = "%#{sanitize(query)}%"
+        # puts "Search ?q= Query: #{query[0..22]} Sanitized: #{q} At #{Time.now}"
+        agency_name_search_results = Agency.where('lower(agency_name) LIKE ?', q)
+        res.write agency_name_search_results.to_json     
+      end
     end
     on "help" do 
       res.write render("./public/home.haml") #, content: "hello, world")
